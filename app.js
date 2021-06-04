4056a6f32b068bc0a0d3cf39009d3c6f69e0d81d
dean <iamdekanos@gmail.com>
2021-06-04 23:48:04 +0300

    Added some comments for extra clarity

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const dotenv = require("dotenv");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const DB_CONNECT = require("./config/db");
-
-dotenv.config({ path: "config/config.env" });
-
 const app = express();
 
+// Load env vars
+dotenv.config({ path: "config/config.env" });
+
+// Connect to DB
 DB_CONNECT();
 
+// Set ejs
 app.set("view engine", "ejs");
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Session middleware
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -26,6 +31,8 @@ app.use(
   })
 );
 
+// Mounted routes
 app.use("/", require("./routes/routes"));
 
+// Server init
 app.listen(5000, console.log(`App is running on port 5000`));
